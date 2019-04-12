91c0718e37ce53ef23008ee6041bd40574fa1eef
roxxette <roxxette@163.com>
2019-04-12 18:55:24 +0800

    update

---
+++ R/Libraries.R
@@ -0,0 +1,21 @@
+
+# Misc Functions of the Department of Statistics, Probability Theory Group (Formerly: E1071),
+library(e1071)
+
+# Recursive Partitioning and Regression Trees
+library(rpart)
+
+# Network Analysis and Visualization
+library(igraph)
+
+# Feed-Forward Neural Networks and Multinomial Log-Linear Models
+library(nnet)
+
+# Breiman and Cutler's Random Forests for Classification and Regression
+library(randomForest)
+
+# Classification and Regression Training
+library(caret)
+
+# Kernel-Based Machine Learning Lab
+library(kernlab)
--- R/build_model.R
+++ R/build_model.R
@@ -1,30 +1,20 @@
 
 
-build_model <- function() {
+train_test_selectivity_model <- function() {
   library(rpart)
 
   df1 <- query_oracle("SELECT SAD_ID, B22_TOTAL_AMOUNT, B25_BORDER_TRANS, B6_TOTAL_PACKAGES, COMPLIANT FROM TRADER_HISTORY")
-  df2 <- query_hive("SELECT SAD_ID, B22_TOTAL_AMOUNT, B25_BORDER_TRANS, B6_TOTAL_PACKAGES, COMPLIANT FROM TRADER_HISTORY")
+  # df2 <- query_hive("SELECT SAD_ID, B22_TOTAL_AMOUNT, B25_BORDER_TRANS, B6_TOTAL_PACKAGES, COMPLIANT FROM TRADER_HISTORY")
 
   df <- df1
   names(df) <- toupper(names(df))
-  # df$y <- ifelse(data[,'COMPLIANT'] == 'OK', 1, 0)
 
-  # model <- rpart(y ~ B22_TOTAL_AMOUNT + B25_BORDER_TRANS + B6_TOTAL_PACKAGES, data = df, method = "class", control=rpart.control(minsplit=1, minbucket=1, cp=0.001))
-  # df$NEW_COLUMN <- df$B22_TOTAL_AMOUNT * df$B6_TOTAL_PACKAGES
   model <- rpart(COMPLIANT ~ B22_TOTAL_AMOUNT + B25_BORDER_TRANS + B6_TOTAL_PACKAGES, data = df, method = "class", control=rpart.control(minsplit=1, minbucket=1, cp=0.001))
 
-  # save(model, file="sysdata.rda")
-  save(model, file="model")
-  # saveRDS(model,file="model")
-
-  #save column names
-  d0 <- data
-  d0$COMPLIANT <- NULL
-  column_names <- colnames(d0)
-
-  write.csv2(colnames(column_names),file="colnames.csv",row.names = FALSE)
-  print("model saved")
+  df$COMPLIANT <- NULL
+  column_names <- colnames(df)
 
   save.image("R/sysdata.rda")
+  print("model saved")
 }
+
