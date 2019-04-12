5a20a0162ead6f80ddedc83276129942ccdaadbc
roxxette <roxxette@163.com>
2019-04-12 12:00:22 +0800

    2

--- R/selModel.R
+++ R/selModel.R
@@ -41,19 +41,21 @@ sel_predict <- function(sad) {
   c_names = as.vector(t(column_names))
   siz = nrow(column_names)
   nrecords = nrow(sad)
-
+print('R0')
   # library(XML)
   # library(RJSONIO)
   # library(jsonlite)
   # df <- fromJSON(sad)
   df <- sad
   names(df) <- toupper(names(df))
-
+print('R1')
   df$B22_TOTAL_AMOUNT <- as.double(df$B22_TOTAL_AMOUNT)
   df$B25_BORDER_TRANS <- as.integer(df$B25_BORDER_TRANS)
   df$B6_TOTAL_PACKAGES <- as.integer(df$B6_TOTAL_PACKAGES)
+print('R2')
 
   result = predict(model, df)
+print('R3')
 
   resultdf = (data.frame(df$SAD_ID, result[2], result[2] > 0.7))
   colnames(resultdf) <- c("sad_id", "score", "alert")
