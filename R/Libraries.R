f7165944a46c8e666e2df188e1efd46b7db9a782
pmp <roxxette@163.com>
2019-04-15 11:47:04 +0800

    Remove libraries

--- R/Libraries.R
+++ R/Libraries.R
@@ -1,21 +1,22 @@
+load_ml_libs <- funtion() {
+  # Misc Functions of the Department of Statistics, Probability Theory Group (Formerly: E1071),
+  library(e1071)
 
-# Misc Functions of the Department of Statistics, Probability Theory Group (Formerly: E1071),
-library(e1071)
+  # Recursive Partitioning and Regression Trees
+  library(rpart)
 
-# Recursive Partitioning and Regression Trees
-library(rpart)
+  # Network Analysis and Visualization
+  library(igraph)
 
-# Network Analysis and Visualization
-library(igraph)
+  # Feed-Forward Neural Networks and Multinomial Log-Linear Models
+  library(nnet)
 
-# Feed-Forward Neural Networks and Multinomial Log-Linear Models
-library(nnet)
+  # Breiman and Cutler's Random Forests for Classification and Regression
+  library(randomForest)
 
-# Breiman and Cutler's Random Forests for Classification and Regression
-library(randomForest)
+  # Classification and Regression Training
+  library(caret)
 
-# Classification and Regression Training
-library(caret)
-
-# Kernel-Based Machine Learning Lab
-library(kernlab)
+  # Kernel-Based Machine Learning Lab
+  library(kernlab)
+}
