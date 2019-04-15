cdc5727ef2cc12a3726ced5e1ea36c90f8b7baab
pmp <roxxette@163.com>
2019-04-15 12:25:07 +0800

    move models to inst

--- R/build_model.R
+++ R/build_model.R
@@ -10,11 +10,11 @@ train_test_selectivity_model <- function() {
 
   library(rpart)
   model <- rpart(COMPLIANT ~ B22_TOTAL_AMOUNT + B25_BORDER_TRANS + B6_TOTAL_PACKAGES, data = df, method = "class", control=rpart.control(minsplit=1, minbucket=1, cp=0.001))
-  saveRDS(model, "models/sel_model")
+  saveRDS(model, "inst/models/sel_model")
 
   df$COMPLIANT <- NULL
   column_names <- colnames(df)
-  saveRDS(column_names, "models/sel_model_columns")
+  saveRDS(column_names, "inst/models/sel_model_columns")
 
   # save.image("R/sysdata.rda")
   print("model saved")
